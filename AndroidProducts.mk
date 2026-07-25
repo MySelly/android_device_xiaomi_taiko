07c774730a92f1c01809c400b197dfb2398a7a88
taiko-dev <dev@local>
2026-07-25 23:27:39 +0300

    taiko: initial tree from yunluo + OS3.0.302.0.WOVMIXM dump

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_taiko.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_taiko-user \
+    lineage_taiko-userdebug \
+    lineage_taiko-eng
---
+++ README.md
@@ -0,0 +1,5 @@
+# Device tree for the Redmi Pad 2 (taiko)
+
+Based on [xiaomi-mt6789-devs/android_device_xiaomi_yunluo](https://github.com/xiaomi-mt6789-devs/android_device_xiaomi_yunluo).
+
+Stock reference: OS3.0.302.0.WOVMIXM (Android 16 / HyperOS 3).
